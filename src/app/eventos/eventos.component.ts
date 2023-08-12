cc752f4ae4ee4e51c78bfddd12ac845636ad7eb1
Caio Cesar <caiocsmeneghelli@outlook.com>
2023-08-12 10:58:37 -0300

    first commit

---
+++ src/app/eventos/eventos.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-eventos',
+  templateUrl: './eventos.component.html',
+  styleUrls: ['./eventos.component.scss'],
+})
+export class EventosComponent implements OnInit {
+  public eventos: any = [];
+  public eventosFiltrados: any = [];
+
+  larguraImg: number = 50;
+  margemImg: number = 2;
+  mostrarImg: boolean = true;
+
+  private _filtroLista: string = '';
+
+  public get filtroLista(){
+    return this._filtroLista;
+  }
+  public set filtroLista(value: string){
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista
+      ? this.filtrarEventos(this.filtroLista)
+      : this.eventos;
+  }
+
+  filtrarEventos(filtrarPor: string): any{
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: any) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor)
+      !== -1
+    )
+  }
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getEventos();
+  }
+
+  alterarImagem() {
+    this.mostrarImg = !this.mostrarImg;
+  }
+  public getEventos(): void {
+    this.http.get('https://localhost:7173/api/eventos').subscribe(
+      (response) => {
+        this.eventos = response
+        this.eventosFiltrados = response
+      },
+      (error) => console.log(error)
+    );
+  }
+}
