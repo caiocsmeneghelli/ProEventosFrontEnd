47d61dadee2bf0df688a95d2337dbcbc9489b20f
Caio Cesar <caiocsmeneghelli@outlook.com>
2023-09-08 18:48:36 -0300

    Adicionando paths

--- src/app/components/eventos/eventos.component.ts
+++ src/app/components/eventos/eventos.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit, TemplateRef } from '@angular/core';
-import { EventoService } from '../../services/evento.service';
-import { Evento } from '../../models/Evento';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { ToastrService } from 'ngx-toastr';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { error } from 'console';
+
+import { EventoService } from '@app/services/evento.service';
+import { Evento } from '@app/models/Evento';
 
 @Component({
   selector: 'app-eventos',
