da1ef6e9929e511e48442825bafad989128fde7a
Caio Cesar <caiocsmeneghelli@outlook.com>
2023-09-08 19:51:25 -0300

    Adicionando validacoes no form de detalhe-evento.

--- src/app/components/eventos/eventos-detalhe/eventos-detalhe.component.ts
+++ src/app/components/eventos/eventos-detalhe/eventos-detalhe.component.ts
@@ -1,29 +1,40 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-eventos-detalhe',
   templateUrl: './eventos-detalhe.component.html',
-  styleUrls: ['./eventos-detalhe.component.scss']
+  styleUrls: ['./eventos-detalhe.component.scss'],
 })
 export class EventosDetalheComponent implements OnInit {
-
   form: FormGroup = this.formBuilder.group({});
-  constructor(private formBuilder:FormBuilder) { }
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
     this.validation();
   }
 
-  public validation(): void{
+  public validation(): void {
     this.form = new FormGroup({
-      tema: new FormControl(),
-      local: new FormControl(),
-      dataEvento: new FormControl(),
-      qtdPessoas: new FormControl(),
-      imagemURL: new FormControl(),
-      telefone: new FormControl(),
-      email: new FormControl()
-    })
+      tema: new FormControl('', [
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(50),
+      ]),
+      local: new FormControl('', Validators.required),
+      dataEvento: new FormControl('', Validators.required),
+      qtdPessoas: new FormControl('', [
+        Validators.required,
+        Validators.max(120000),
+      ]),
+      telefone: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      imagemURL: new FormControl('', Validators.required),
+    });
   }
 }
