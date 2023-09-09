f56913421ec653fbaec3c7c5c17cc1f1cda3d7bf
Caio Cesar <caiocsmeneghelli@outlook.com>
2023-09-08 22:25:11 -0300

    adicionando validacao de senha e confirmacao de senha.

--- src/app/components/user/registration/registration.component.ts
+++ src/app/components/user/registration/registration.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControlOptions, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidatorField } from '@app/helpers/ValidatorField';
 
 @Component({
   selector: 'app-registration',
@@ -19,13 +20,16 @@ export class RegistrationComponent implements OnInit {
   }
 
   private validation(): void {
+    const formOptions: AbstractControlOptions = {
+      validators: ValidatorField.MustMatch('senha', 'confirmeSenha')
+    }
     this.form = this.fb.group({
       primeiroNome: ['', Validators.required],
       ultimoNome: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
       userName: ['', Validators.required],
       senha: ['', [Validators.required, Validators.minLength(6)]],
-      confirmeSenha: ['', [Validators.required, Validators.minLength(6)]],
-    });
+      confirmeSenha: ['', Validators.required],
+    }, formOptions);
   }
 }
