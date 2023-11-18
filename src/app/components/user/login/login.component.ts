b24851e5203ccb84b01a7b5ca65d46df3c9ef7cb
Caio Cesar <caiocsmeneghelli@outlook.com>
2023-11-18 17:52:05 -0300

    Adicionando login.

--- src/app/components/user/login/login.component.ts
+++ src/app/components/user/login/login.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserLogin } from '@app/models/Identity/UserLogin';
+import { UserService } from '@app/services/user.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  model = {} as UserLogin;
+  constructor(
+    private userService: UserService,
+    private router: Router,
+    private toastr: ToastrService
+  ) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  public login(): void {
+    this.userService.login(this.model).subscribe(
+      () => {
+        this.router.navigateByUrl('/dashboard');
+      },
+      (error: any) => {
+        if (error.status == 401) {
+          this.toastr.error('Usuário ou senha inválido');
+        } else {
+          console.error(error);
+        }
+      }
+    );
   }
-
 }
