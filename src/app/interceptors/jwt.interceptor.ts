d1a4de2dbfd6d316a0df3df7aa9f54c1beeea63d
Caio Cesar <caiocsmeneghelli@outlook.com>
2023-11-19 21:53:56 -0300

    Ajustes referente a problema de token incorreto sendo enviado.

--- src/app/interceptors/jwt.interceptor.ts
+++ src/app/interceptors/jwt.interceptor.ts
@@ -17,20 +17,26 @@ export class JwtInterceptor implements HttpInterceptor {
     request: HttpRequest<unknown>,
     next: HttpHandler
   ): Observable<HttpEvent<unknown>> {
-    let currentUser = {} as User;
+    let currentUser = this.userService.getUser();
+    if(currentUser){
+      request = request.clone({
+        setHeaders: {
+          Authorization: `Bearer ${currentUser.token}`
+        }
+      })
+    };
 
-    this.userService.currentUser$.pipe(take(1)).subscribe((user) => {
-      console.log(currentUser);
-      currentUser = user;
-      if (currentUser) {
-        console.log(`teste - ${currentUser.token}`)
-        request = request.clone({
-          setHeaders: {
-            Authorization: `Bearer ${currentUser.token}`,
-          },
-        });
-      }
-    });
+    // let currentUser = {} as User;
+    // this.userService.currentUser$.pipe(take(1)).subscribe((user) => {
+    //   currentUser = user;
+    //   if (currentUser) {
+    //     request = request.clone({
+    //       setHeaders: {
+    //         Authorization: `Bearer ${currentUser.token}`,
+    //       },
+    //     });
+    //   }
+    // });
 
     return next.handle(request);
   }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -48,4 +48,9 @@ export class UserService {
     this.currentUserSource.next(user);
     this.currentUserSource.complete();
   }
+
+  public getUser() : User{
+    const user = JSON.parse(localStorage.getItem('user')!) as User;
+    return user;
+  }
 }
