dac173a2b59dd3a482ca832a801813003e5bf883
Caio Cesar <caiocsmeneghelli@outlook.com>
2024-03-24 09:33:52 -0300

    add ajustes

--- src/app/components/palestrantes/palestrante-detalhe/palestrante-detalhe.component.ts
+++ src/app/components/palestrantes/palestrante-detalhe/palestrante-detalhe.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Palestrante } from '@app/models/Palestrante';
 import { PalestranteService } from '@app/services/palestrante.service';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { ToastrService } from 'ngx-toastr';
@@ -8,60 +9,80 @@ import { debounceTime, map, tap } from 'rxjs';
 @Component({
   selector: 'app-palestrante-detalhe',
   templateUrl: './palestrante-detalhe.component.html',
-  styleUrls: ['./palestrante-detalhe.component.scss']
+  styleUrls: ['./palestrante-detalhe.component.scss'],
 })
 export class PalestranteDetalheComponent implements OnInit {
   public form!: FormGroup;
   public situacaoForm = '';
   public corDescricao = '';
 
-
   constructor(
     private fb: FormBuilder,
     public palestranteService: PalestranteService,
     private toastr: ToastrService,
     private spinner: NgxSpinnerService
-  ) { }
+  ) {}
 
   ngOnInit() {
+    this.carregarPalestrante();
     this.validation();
     this.verificaForm();
   }
 
-  private validation(): void{
+  private validation(): void {
     this.form = this.fb.group({
-      miniCurriculo: ['']
+      miniCurriculo: [''],
     });
   }
 
-  private verificaForm(): void{
+  private verificaForm(): void {
     this.form.valueChanges
-    .pipe(
-      map(() => {
-        this.situacaoForm = 'Minicurriculo está sendo Atualizado';
-        this.corDescricao = 'text-warning';
-      }),
-      debounceTime(1000),
-      tap(() => this.spinner.show())
-    ).subscribe(
-      () => {
-        this.palestranteService.put({...this.form.value})
-        .subscribe(
-          () => {
-            this.situacaoForm = 'Minicurriculo foi atualizado';
-            this.corDescricao = 'text-success';
-          },
-          (error) => {
-            this.toastr.error('Erro ao tentar atualizar Palestrante.', 'Erro');
-            console.log(error);
-          }
-        ).add(() => this.spinner.hide());
-
-      }
+      .pipe(
+        map(() => {
+          this.situacaoForm = 'Minicurriculo está sendo Atualizado';
+          this.corDescricao = 'text-warning';
+        }),
+        debounceTime(1000),
+        tap(() => this.spinner.show())
       )
+      .subscribe(() => {
+        this.palestranteService
+          .put({ ...this.form.value })
+          .subscribe(
+            () => {
+              this.situacaoForm = 'Minicurriculo foi Atualizado';
+              this.corDescricao = 'text-success';
+
+              setTimeout(() => {
+                this.situacaoForm = 'Minicurriculo carregado!';
+                this.corDescricao = 'text-muted';
+              }, 2000)
+            },
+            (error) => {
+              this.toastr.error(
+                'Erro ao tentar atualizar Palestrante.',
+                'Erro'
+              );
+              console.log(error);
+            }
+          )
+          .add(() => this.spinner.hide());
+      });
   }
 
-  public get f(): any{
+  private carregarPalestrante(): void{
+    this.spinner.show();
+    this.palestranteService.getPalestrante()
+    .subscribe(
+      (palestrante: Palestrante) =>{
+        this.form.patchValue(palestrante);
+      },
+      (error: any) => {
+        this.toastr.error('Falha ao carregar Palestrante.', 'Erro');
+      }
+    ).add(() => this.spinner.hide());
+  }
+  public get f(): any {
     return this.form.controls;
   }
 }
